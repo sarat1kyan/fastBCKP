dd152fcfda274984731214d11c624cfc9a4a0791
saratikyan <91123731+54R4T1KY4N@users.noreply.github.com>
2023-08-15 02:17:23 +0400

    fastBCKP.sh

--- fastBCKP.sh
+++ fastBCKP.sh
@@ -2,62 +2,12 @@
 
 echo "let's start"
 
-banner1() {
-  local text="$@"
-  local length=$(( ${#text} + 2 ))
-  local line=$(printf '%*s' "$length" '' | tr ' ' '-')
-  echo "+$line+"
-  printf "| %s |\n" "$(date)"
-  echo "+$line+"
-  printf "|$bold%s$reset|\n" "$text"
-  echo "+$line+"
-}
-
-# Check if script is being run as root
-if [[ $EUID -ne 0 ]]; then
-  banner1 "This script must be run as root."
-  exit 1
-fi
-
-#clolors
-white='\e[1;37m'
-green='\e[0;32m'
-blue='\e[1;34m'
-red='\e[1;31m'
-yellow='\e[1;33m' 
-echo "_"
-echo "_"
-banner() {
-	echo -e $'\e[1;33m\e[0m\e[1;37m       █████▒▄▄▄        ██████ ▄▄▄█████▓ ▄▄▄▄    ▄████▄   ██ ▄█▀ ██▓███     \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m     ▓██   ▒▒████▄    ▒██    ▒ ▓  ██▒ ▓▒▓█████▄ ▒██▀ ▀█   ██▄█▒ ▓██░  ██▒   \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m     ▒████ ░▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░▒██▒ ▄██▒▓█    ▄ ▓███▄░ ▓██░ ██▓▒   \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m     ░▓█▒  ░░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒██░█▀  ▒▓▓▄ ▄██▒▓██ █▄ ▒██▄█▓▒ ▒   \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m     ░▓█▒  ░░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒██░█▀  ▒▓▓▄ ▄██▒▓██ █▄ ▒██▄█▓▒ ▒   \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m      ▒ ░    ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   ░▒▓███▀▒░ ░▒ ▒  ░▒ ▒▒ ▓▒▒▓▒░ ░  ░   \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m      ░       ▒   ▒▒ ░░ ░▒  ░ ░    ░    ▒░▒   ░   ░  ▒   ░ ░▒ ▒░░▒ ░        \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m      ░ ░     ░   ▒   ░  ░  ░    ░       ░    ░ ░        ░ ░░ ░ ░░          \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m                  ░  ░      ░            ░      ░ ░      ░  ░               \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m                                              ░ ░                           \e[0m'
-	
-	
-	echo""  
-	echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m backup anything with one command \e[0m''\e[1;37m\e[0m\e[1;37m \e[0m'                                       
-	echo ""
-	echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/54R4T1KY4N \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
-	echo ""
-	echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+ >>\e[0m'
-	echo -e "\e[93m    fastBCKP |2|.|1| stable"      
-	echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+ >>\e[0m' 
-	echo ""                                                
-}
-banner 
-
 # Ask user for directory path to backup
 read -p "Enter directory path to backup: " directory
 
 # Check if directory exists
 if [ ! -d "$directory" ]; then
-  banner1 "Error: Directory not found!"
+  echo "Error: Directory not found!"
   exit 1
 fi
 # Prompt the user to set a password
@@ -81,9 +31,9 @@ if [[ $schedule_choice =~ ^[Yy]$ ]]; then
     fi
     sleep_duration=$((backup_timestamp - current_timestamp))
     at "$backup_time" <<< "bash backup.sh \"$directory\" \"$password\" \"$upload_destination\""
-    banner1 "Backup of $directory scheduled for $backup_time. Archive will be saved to $HOME/backups."
-    banner1 "You may now close the script. The backup task will run automatically at the scheduled time."
-    banner1 "Sleeping for $sleep_duration seconds until backup time."
+    echo "Backup of $directory scheduled for $backup_time. Archive will be saved to $HOME/backups."
+    echo "You may now close the script. The backup task will run automatically at the scheduled time."
+    echo "Sleeping for $sleep_duration seconds until backup time."
     sleep $sleep_duration >/dev/null 2>&1
     exit 0
 fi
@@ -108,12 +58,12 @@ if [[ $upload_choice =~ ^[Yy]$ ]]; then
     read -p "Enter the upload destination (user@hostname:/path/to/destination): " upload_destination
     scp "$backup_dir/$directory.tar.gz.enc" "$upload_destination" >/dev/null 2>&1
      if [ $? -eq 0 ]; then
-        banner1 "Backup archive uploaded to $upload_destination."
+        echo "Backup archive uploaded to $upload_destination."
      else
-        banner1 "Error: Failed to upload backup archive."
+        echo "Error: Failed to upload backup archive."
   fi
 
 fi
 
 # Print a success message to the user
-banner1 "Backup of $directory completed successfully. Archive saved to $backup_dir."
+echo "Backup of $directory completed successfully. Archive saved to $backup_dir."
